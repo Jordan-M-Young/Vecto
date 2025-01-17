2a736cf2ae5742dfce9f9d343d1c5d46c4dfe322
jordan-m-young <jordan.m.young@gmail.com>
2025-01-16 18:04:42 -0600

    dec tree new

--- src/models/classifier.rs
+++ src/models/classifier.rs
@@ -18,3 +18,14 @@ pub enum TreeNodes {
     TreeNode,
     NullNode,
 }
+
+
+impl DecisionTree{
+    pub fn new(min_samples: u32, min_depth: u32) -> DecisionTree {
+        DecisionTree{
+            _min_samples: min_samples,
+            _max_depth: min_depth
+        }
+    }
+}
+
